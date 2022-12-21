3db3c7f8732f4f8455354f68ff0a719afe162452
Ahmed Hany <95107740+AhmedAAst@users.noreply.github.com>
2022-12-21 10:08:22 +0200

    Update Test.c

--- Project_2/MCAL/Dio_Driver/Test.c
+++ Project_2/MCAL/Dio_Driver/Test.c
@@ -1,25 +1,25 @@
-#include "../../Application/Application.h"
-int main(void)
-{
-	while (1)
-	{
-		Dio_init(PORT_A, PIN0, OUT);/*RUN*/
-		Dio_init(PORT_A, PIN1, IN);/*NO*/
-		Dio_init(PORT_A, PIN2, OUT);/*RUN*/
-		Dio_init(PORT_B, PIN2, IN);/*RUN*/
+//#include "../../Application/Application.h"
+//int main(void)
+//{
+//	while (1)
+//	{
+	//	Dio_init(PORT_A, PIN0, OUT);/*RUN*/
+	//	Dio_init(PORT_A, PIN1, IN);/*NO*/
+		//Dio_init(PORT_A, PIN2, OUT);/*RUN*/
+		//Dio_init(PORT_B, PIN2, IN);/*RUN*/
 
-		Two_led_Toogle(PORT_A,PIN1,PORT_A,PIN0);
-		Two_led_Toogle(PORT_A,PIN2,PORT_B,PIN2);
-
-
-		uint8_t Status = 0;
-		Dio_read(PORT_A, PIN1, &Status);
-		if(Status)
-		{
-			Dio_write(PORT_A, PIN2, LOW);
-		}
-	}
+		//Two_led_Toogle(PORT_A,PIN1,PORT_A,PIN0);
+		//Two_led_Toogle(PORT_A,PIN2,PORT_B,PIN2);
 
 
+//uint8_t Status = 0;
+		//Dio_read(PORT_A, PIN1, &Status);
+		//if(Status)
+		//{
+		//	Dio_write(PORT_A, PIN2, LOW);
+		//}
+	//}
 
-	}
+
+
+	//}
