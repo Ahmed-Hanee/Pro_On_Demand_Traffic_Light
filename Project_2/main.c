67a5f2944ec0f0cebae85d0e2c8be3a8cac8aa1c
Ahmed Hany <95107740+AhmedAAst@users.noreply.github.com>
2022-12-21 10:09:28 +0200

    Update main.c

--- Project_2/main.c
+++ Project_2/main.c
@@ -1,12 +1,12 @@
-// #include <avr/io.h>
-// #include "Application/Application.h"
-// int main(void)
-// {
-// 	APP_init();
-// 
-//     while (1) 
-//     {
-// Start() ;
-//     }
-// }
-// 
+ #include <avr/io.h>
+ #include "Application/Application.h"
+ int main(void)
+ {
+ 	APP_init();
+ 
+   while (1) 
+   {
+ Start() ;
+    }
+ }
+
